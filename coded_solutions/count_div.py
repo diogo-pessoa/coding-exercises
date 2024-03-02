f7811fd9fa33f215f1b5417c6de2412c1735404d
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2024-03-02 12:35:26 +0000

    lesson 5.3 done

--- coded_solutions/count_div.py
+++ coded_solutions/count_div.py
@@ -1,4 +1,16 @@
-def solution_1(a: int, b: int, c: int) -> int:
+def solution_bad_time_complexity(a: int, b: int, c: int) -> int:
     range_div = range(a, b+1)
     mod_k = [x % c for x in range_div]
     return mod_k.count(0)
+
+def solution_1(x: int, y: int, K: int) -> int:
+    if x % K == 0:
+        first_divisible = x
+    else:
+        first_divisible = x + (K - x % K)
+    last_divisible = y - y % K
+
+    if first_divisible > y:
+        return 0
+
+    return (last_divisible - first_divisible) // K + 1
