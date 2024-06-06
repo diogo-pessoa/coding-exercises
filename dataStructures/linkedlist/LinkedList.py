02458efa95bbebc74fc5339b1a9cc7e77d0540bf
diogo-pessoa <diogo-pessoa@git.com>
2024-06-06 11:23:46 +0100

    Data Structures training LinkedLists

--- dataStructures/linkedlist/LinkedList.py
+++ dataStructures/linkedlist/LinkedList.py
@@ -9,7 +9,6 @@ class Node:
 
 
 class LinkedList:
-
     def __init__(self, data):
         """
         The LL constructor is creating a new node,
@@ -22,8 +21,106 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    def get_by_index(self, index):
+        """
+        O(n)
+        :param index:
+        :return:
+        """
+        if index < 0 or index >= self.length:  # remember to validate the index is within length of LL
+            return None
+        node = self.head
+        for i in range(index):
+            node = node.next
+        return node
+
+    def get_head(self):
+        return self.head if self.head else None
+
+    def get_tail(self):
+        return self.tail if self.tail else None
+
+    def get_length(self):
+        return self.length
+
+    def is_empty(self):
+        return self.length == 0
+
+    def update_node_value(self, index, data):
+        """
+        O(n)
+        :param index:
+        :param data:
+        :return:
+        """
+        if index < 0 or index >= self.length:
+            return None
+        node = self.get_by_index(index)  # O(n) using the get_by_index method to get the node
+        if node:  # if node is found, update the data
+            node.data = data  # update the data of the node
+        return node
+
+    def insert_new_node(self, data, index):
+        """
+        O(n)
+        :param data:
+        :param index:
+        :return:
+        """
+        if index < 0 or index >= self.length:  # Index Out of Bounds
+            return None
+        if index == self.length:  # Inserting at the end of the LL
+            return self.append(data)  # O(1)
+
+        if index == 0:  # First element in LL
+            return self.prepend(data)  # O(1)
+
+        # Inserting in the middle of the LL
+        new_node = Node(data)
+        node = self.get_by_index(index - 1)  # O(n)
+        new_node.next = node.next
+        node.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        """
+        For the return value it made sense to return the removed node, instead of a Boolean.
+        In cases the client uses this values later.
+        O(n)
+        :returns: removed_node: Node or None
+        """
+        if index < 0 or index >= self.length:  # Index Out of Bounds
+            return None
+        if index == 0:  # First element in LL
+            return self.pop_head()
+        if index == self.length - 1:  # Last element in LL
+            return self.pop()
+        previous_node_in_ll = self.get_by_index(index - 1)  # O(n)
+        removed_node = previous_node_in_ll.next
+        previous_node_in_ll.next = removed_node.next  # O(1) - Updating the next value of preceding deleted node
+        self.length -= 1
+        return removed_node
+
+    def reverse(self):
+        """
+        O(n) - Need to iterate through the whole list to reverse the order of the nodes.
+
+        :return:
+        """
+        previous = None
+        current = self.head  # Starting at the first node
+        self.tail = self.head  # setting the last node as the former head
+        while current is not None:  # Tail node doesnt a .next value
+            next_node = current.next
+            current.next = previous  # first interation will be None
+            previous = current
+            current = next_node
+        self.head = previous
+
     def append(self, data):
         """
+        O(1)
         Add value to end of LL.
         - Need to update last element (tail)
         - bump the length
@@ -41,30 +138,11 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
-        return self.tail
-
-    def prepend(self, data):
-        """
-        Adding a new node to the beginning of the LL
-        - move head data to new node
-        - set next value to former head
-        - increment the length of the ll
-        :param data:
-        :return: node: Node
-        """
-        new_node = Node(data)
-        if not self.head:  # if list is empty.
-            self.head = new_node
-            self.tail = new_node
-
-        node = Node(data)
-        node.next = self.head.data
-        self.head = node
-        self.length += 1
-        return node
+        return True
 
     def pop(self):
         """
+        O(1) - worst case we navigate the whole list, see while l.57
         Update new tail by checking if .next in element is current self.tail.
         :return:
         """
@@ -77,9 +155,49 @@ class LinkedList:
                 new_tail = new_tail.next
             self.tail = new_tail
             self.tail.next = None
-        return self.tail
+        return self.tail  # returning the removed tail node
+
+    def prepend(self, data):
+        """
+        O(1)
+        Adding a new node to the beginning of the LL
+        - move head data to new node
+        - set next value to former head
+        - increment the length of the ll
+        :param data:
+        """
+        new_node = Node(data)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            node = Node(data)
+            node.next = self.head.data
+            self.head = node
+            self.length += 1
+        return True
+
+    def pop_head(self):
+        """
+        O(1)
+        :return: head_node: returns the removed head node
+        """
+        removed_head = self.head
+        new_head = self.head.next
+        if self.length == 0:
+            return None
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return None
+
+        self.head = new_head
+        self.length -= 1
+        return removed_head  # returning the removed head node
 
     def print_list(self):
+
         """
         Prints all elements in the list.
         Starting from the head element and updating the node with .next until tail element (next will be None)
--- dataStructures/linkedlist/test_LinkedList.py
+++ dataStructures/linkedlist/test_LinkedList.py
@@ -20,18 +20,80 @@ class TestLinkedList(TestCase):
     def test_prepend_ll(self):
         linked_list = LinkedList(3)
         linked_list.prepend(2)
-        self.assertEqual(linked_list.head.data, 2)
-        self.assertEqual(linked_list.head.next, 3)
+        self.assertEqual(linked_list.get_head().data, 2)
+        self.assertEqual(linked_list.get_head().next, 3)
 
     def test_append_ll(self):
         linked_list = LinkedList(3)
         linked_list.append(2)
-        linked_list.print_list()
-        self.assertEqual(linked_list.tail.data, 2)
+        self.assertEqual(linked_list.get_tail().data, 2)
 
     def test_pop_ll(self):
         linked_list = LinkedList(3)
         linked_list.append(2)
         linked_list.pop()
-        self.assertEqual(linked_list.tail.data, 3)
-        self.assertEqual(linked_list.tail.next, None)
+        self.assertEqual(linked_list.get_tail().data, 3)
+        self.assertEqual(linked_list.get_tail().next, None)
+
+    def test_ll_empty(self):
+        linked_list = LinkedList(3)
+        linked_list.pop()
+        self.assertTrue(linked_list.is_empty())
+
+    def test_pop_head_ll(self):
+        linked_list = LinkedList(3)
+        linked_list.append(2)
+        linked_list.append(4)
+        linked_list.pop_head()
+        self.assertEqual(linked_list.get_head().data, 2)
+        self.assertEqual(linked_list.get_head().next.data, 4)
+
+    def test_get_head(self):
+        self.assertEqual(LinkedList(3).get_head().data, 3)
+
+    def test_get_tail(self):
+        self.assertEqual(LinkedList(3).get_tail().data, 3)
+
+    def test_get_length(self):
+        linked_list = LinkedList(3)
+        linked_list.append(2)
+        linked_list.append(4)
+        self.assertEqual(linked_list.get_length(), 3)
+
+    def test_get_by_index(self):
+        linked_list = LinkedList(3)
+        linked_list.append(2)
+        linked_list.append(4)
+        self.assertEqual(linked_list.get_by_index(1).data, 2)
+
+    def test_update_node_value(self):
+        linked_list = LinkedList(3)
+        linked_list.append(2)
+        linked_list.append(4)
+        linked_list.update_node_value(1, 5)
+        self.assertEqual(linked_list.get_by_index(1).data, 5)
+
+    def test_insert_new_node(self):
+        linked_list = LinkedList(3)
+        linked_list.append(2)
+        linked_list.append(4)
+        linked_list.insert_new_node(5, 1)
+        self.assertEqual(linked_list.get_by_index(1).data, 5)
+
+    def test_remove_node(self):
+        linked_list = LinkedList(3)
+        linked_list.append(2)
+        linked_list.append(4)
+        linked_list.remove(1)
+        self.assertEqual(linked_list.get_by_index(1).data, 4)
+
+    def test_reverse_ll(self):
+        linked_list = LinkedList(3)
+        linked_list.append(2)
+        linked_list.append(4)
+        linked_list.print_list()
+        linked_list.reverse()
+        linked_list.print_list()
+        self.assertEqual(linked_list.get_by_index(0).data, 4)
+        self.assertEqual(linked_list.get_by_index(1).data, 2)
+        self.assertEqual(linked_list.get_by_index(2).data, 3)
