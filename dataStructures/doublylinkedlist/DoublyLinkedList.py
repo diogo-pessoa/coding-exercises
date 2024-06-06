a359c2bb61a4d5b688f371bfc9bb31f0b44a2a8c
diogo-pessoa <diogo-pessoa@git.com>
2024-06-06 14:29:28 +0100

    get_nodes by index, leveraging the .prev in case index in second half of list

--- dataStructures/doublylinkedlist/DoublyLinkedList.py
+++ dataStructures/doublylinkedlist/DoublyLinkedList.py
@@ -45,5 +45,69 @@ class DoublyLinkedList:
             new_tail = new_tail.next
         self.tail = new_tail
         self.tail.next = None
-
+        self.size -= 1
         return True
+
+    def prepend(self, data):
+        """
+        O(1)
+        :param data:
+        :return:
+        """
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head.prev = new_node
+        self.head = new_node
+        self.size += 1
+        return True
+
+    def pop_first(self):
+        """
+        O(1)
+        :return:
+        """
+        if self.size == 0:
+            return None
+        if self.size == 1:
+            self.head = None
+            self.tail = None
+            self.size = 0
+            return None
+        self.head = self.head.next
+        self.head.prev = None
+        self.size -= 1
+        return True
+
+    def get_node(self, index):
+        """
+        O(n)
+        :param index:
+        :return:
+        """
+        if index < 0 or index >= self.size:
+            return None
+
+        # for a doubly linked list, we can traverse from the head or tail,
+        # we can evaluate the index against the length of the list
+        if index < self.size // 2: # if index is less than half the size of the list, traverse from the head
+            node = self.head
+            for i in range(index):
+                node = node.next
+        else: # if index is greater than half the size of the list, traverse from the tail
+            node = self.tail
+            for i in range(self.size - 1, index, -1):
+                node = node.prev
+        return node
+    def __str__(self):
+        """
+        O(n)
+        :return:
+        """
+        if self.size == 0:
+            return "Empty list"
+        current_node = self.head
+        output = []
+        while current_node:
+            output.append(current_node.data)
+            current_node = current_node.next
+        return f"{' -> '.join(map(str, output))}"
--- dataStructures/doublylinkedlist/test_DoublyLinkedList.py
+++ dataStructures/doublylinkedlist/test_DoublyLinkedList.py
@@ -15,12 +15,15 @@ class TestDoublyLinkedList(TestCase):
         d_linked_list.append(20)
         self.assertEqual(d_linked_list.head.next.data, 20)
         self.assertEqual(d_linked_list.tail.prev.data, 10)
+        self.assertEqual(d_linked_list.size, 2)
 
     @unittest.skip("Not implemented")
     def test_append_empty_list(self):
         d_linked_list = DoublyLinkedList(10)
         d_linked_list.append(20)
         self.assertEqual(d_linked_list.head.next.data, 20)
+        self.assertEqual(d_linked_list.tail.prev.data, 10)
+        self.assertEqual(d_linked_list.size, 2)
 
     def test_pop(self):
         d_linked_list = DoublyLinkedList(10)
@@ -30,6 +33,7 @@ class TestDoublyLinkedList(TestCase):
         self.assertEqual(d_linked_list.tail.data, 20)
         self.assertEqual(d_linked_list.tail.next, None)
         self.assertEqual(d_linked_list.tail.prev.data, 10)
+        self.assertEqual(d_linked_list.size, 2)
 
     def test_pop_one_element(self):
         d_linked_list = DoublyLinkedList(10)
@@ -37,3 +41,40 @@ class TestDoublyLinkedList(TestCase):
         self.assertEqual(d_linked_list.tail, None)
         self.assertEqual(d_linked_list.head, None)
         self.assertEqual(d_linked_list.size, 0)
+
+    def test_prepend(self):
+        d_linked_list = DoublyLinkedList(10)
+        d_linked_list.prepend(5)
+        self.assertEqual(d_linked_list.head.data, 5)
+        self.assertEqual(d_linked_list.head.next.data, 10)
+        self.assertEqual(d_linked_list.head.next.prev.data, 5)
+        self.assertEqual(d_linked_list.size, 2)
+
+    def test_pop_first(self):
+        d_linked_list = DoublyLinkedList(10)
+        d_linked_list.append(20)
+        d_linked_list.append(30)
+        d_linked_list.pop_first()
+        self.assertEqual(d_linked_list.head.data, 20)
+        self.assertEqual(d_linked_list.head.prev, None)
+        self.assertEqual(d_linked_list.size, 2)
+
+    def test_str_method(self):
+        d_linked_list = DoublyLinkedList(10)
+        d_linked_list.append(20)
+        d_linked_list.append(30)
+        print(d_linked_list)
+        self.assertEqual(str(d_linked_list), "10 -> 20 -> 30")
+
+    def test_get_node(self):
+        """
+        Running this test with the Debugger helps to see the iteration through head or tail
+        :return:
+        """
+        d_linked_list = DoublyLinkedList(10)
+        d_linked_list.append(20)
+        d_linked_list.append(30)
+        self.assertEqual(d_linked_list.get_node(1).data, 20)
+        self.assertEqual(d_linked_list.get_node(2).data, 30)
+        self.assertEqual(d_linked_list.get_node(0).data, 10)
+        self.assertEqual(d_linked_list.get_node(3), None)
